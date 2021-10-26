56622da6760e751ed600f31adbd8afbf66abc718
KHOLKIN-V <kholkinva@yandex.ru>
2021-10-26 18:11:36 +0400

    upd 3

---
+++ src/components/FriendItem/FriendItem.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import cs from "./FriendItem.module.css";
+
+const FriendItem = ({ isOnline, name, avatar }) => {
+  return (
+    <>
+      <span
+        className={cs.status}
+        style={
+          isOnline ? { backgroundColor: "green" } : { backgroundColor: "red" }
+        }
+      >
+        {isOnline}
+      </span>
+      <img className={cs.avatar} src={avatar} alt={name} width="48" />
+      <p className={cs.name}>{name}</p>
+    </>
+  );
+};
+
+export default FriendItem;
+
+FriendItem.propTypes = {
+  friend: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      avatar: PropTypes.string.isRequired,
+    })
+  ),
+};
--- src/components/Friendlist/FriendList.jsx
+++ src/components/Friendlist/FriendList.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import cs from "./FriendList.module.css";
+import FriendItem from "../FriendItem/FriendItem";
 
 const FriendList = ({ friend }) => {
   return (
@@ -7,18 +8,7 @@ const FriendList = ({ friend }) => {
       <ul className={cs.Friendlist}>
         {friend.map(({ id, isOnline, name, avatar }) => (
           <li className={cs.item} key={id}>
-            <span
-              className={cs.status}
-              style={
-                isOnline
-                  ? { backgroundColor: "green" }
-                  : { backgroundColor: "red" }
-              }
-            >
-              {isOnline}
-            </span>
-            <img className={cs.avatar} src={avatar} alt={name} width="48" />
-            <p className={cs.name}>{name}</p>
+            <FriendItem isOnline={isOnline} name={name} avatar={avatar} />
           </li>
         ))}
       </ul>
@@ -30,9 +20,6 @@ FriendList.propTypes = {
   friend: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      avatar: PropTypes.string.isRequired,
     })
   ),
 };
